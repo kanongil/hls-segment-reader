c759cbb34bafadd4569bb68216c76868f0ad6710
Gil Pedersen <git@gpost.dk>
2015-08-04 11:39:46 +0200

    Change indent to 4 spaces

--- segment-reader.js
+++ segment-reader.js
@@ -17,342 +17,342 @@ var internals = {};
 
 internals.fetchFrom = function (reader, seqNo, segment, callback) {
 
-  var segmentUrl = Url.resolve(reader.baseUrl, segment.uri);
-  var probe = !reader.withData;
+    var segmentUrl = Url.resolve(reader.baseUrl, segment.uri);
+    var probe = !reader.withData;
 
-  var streamOptions = { probe: probe, highWaterMark: 100 * 1000 * 1000 };
-  if (segment.byterange) {
-    streamOptions.start = segment.byterange.offset;
-    streamOptions.end = segment.byterange.offset + segment.byterange.length - 1;
-  }
-
-  var stream = UriStream(segmentUrl, streamOptions);
-
-  var finish = function (err, res) {
-
-    stream.removeListener('meta', onmeta);
-    stream.removeListener('end', onfail);
-    stream.removeListener('error', onfail);
-    callback(err, res);
-  };
-
-  var onmeta = function (meta) {
-
-    if (reader.segmentMimeTypes.indexOf(meta.mime.toLowerCase()) === -1) {
-      if (stream.abort) {
-        stream.abort();
-      }
-      return stream.emit('error', new Error('Unsupported segment MIME type: ' + meta.mime));
+    var streamOptions = { probe: probe, highWaterMark: 100 * 1000 * 1000 };
+    if (segment.byterange) {
+        streamOptions.start = segment.byterange.offset;
+        streamOptions.end = segment.byterange.offset + segment.byterange.length - 1;
     }
 
-    finish(null, new exports.HlsSegmentObject(seqNo, segment, meta, probe ? null : stream));
-  };
+    var stream = UriStream(segmentUrl, streamOptions);
 
-  var onfail = function (err) {
+    var finish = function (err, res) {
 
-    if (!err) {
-      err = new Error('No metadata');
-    }
+        stream.removeListener('meta', onmeta);
+        stream.removeListener('end', onfail);
+        stream.removeListener('error', onfail);
+        callback(err, res);
+    };
 
-    finish(err);
-  };
+    var onmeta = function (meta) {
 
-  stream.on('meta', onmeta);
-  stream.on('end', onfail);
-  stream.on('error', onfail);
+        if (reader.segmentMimeTypes.indexOf(meta.mime.toLowerCase()) === -1) {
+            if (stream.abort) {
+                stream.abort();
+            }
+            return stream.emit('error', new Error('Unsupported segment MIME type: ' + meta.mime));
+        }
 
-  return stream;
+        finish(null, new exports.HlsSegmentObject(seqNo, segment, meta, probe ? null : stream));
+    };
+
+    var onfail = function (err) {
+
+        if (!err) {
+            err = new Error('No metadata');
+        }
+
+        finish(err);
+    };
+
+    stream.on('meta', onmeta);
+    stream.on('end', onfail);
+    stream.on('error', onfail);
+
+    return stream;
 };
 
 
 internals.checkNext = function checknext (reader) {
 
-  var state = reader.readState;
-  var index = reader.index;
-  if (!reader.readable || !state.active || state.fetching || state.nextSeq === -1 || !index) {
-    return null;
-  }
-
-  var seq = state.nextSeq;
-  var segment = index.getSegment(seq, true);
-
-  if (segment) {
-    // mark manual discontinuities
-    if (state.discont) {
-      segment.discontinuity = true;
-      state.discont = false;
+    var state = reader.readState;
+    var index = reader.index;
+    if (!reader.readable || !state.active || state.fetching || state.nextSeq === -1 || !index) {
+        return null;
     }
 
-    // check if we need to stop
-    if (reader.stopDate && segment.program_time > reader.stopDate) {
-      return reader.push(null);
-    }
+    var seq = state.nextSeq;
+    var segment = index.getSegment(seq, true);
 
-    state.fetching = internals.fetchFrom(reader, seq, segment, function (err, object) {
-
-      if (!reader.readable) {
-        return;
-      }
-
-      state.fetching = null;
-      if (err) {
-        reader.emit('error', err);
-      }
-
-      if (seq === state.nextSeq) {
-        state.nextSeq++;
-      }
-
-      if (object) {
-        if (object.stream) {
-          reader.watch[object.seq] = object.stream;
-          Oncemore(object.stream).once('end', 'error', function () {
-
-            delete reader.watch[object.seq];
-          });
+    if (segment) {
+        // mark manual discontinuities
+        if (state.discont) {
+            segment.discontinuity = true;
+            state.discont = false;
         }
 
-        state.active = reader.push(object);
-      }
+        // check if we need to stop
+        if (reader.stopDate && segment.program_time > reader.stopDate) {
+            return reader.push(null);
+        }
 
-      internals.checkNext(reader);
-    });
-  } else if (index.ended) {
-    reader.push(null);
-  } else if (!index.type && (index.lastSeqNo() < state.nextSeq - 1)) {
-    // handle live stream restart
-    state.discont = true;
-    state.nextSeq = index.startSeqNo(true);
-    internals.checkNext(reader);
-  }
+        state.fetching = internals.fetchFrom(reader, seq, segment, function (err, object) {
+
+            if (!reader.readable) {
+                return;
+            }
+
+            state.fetching = null;
+            if (err) {
+                reader.emit('error', err);
+            }
+
+            if (seq === state.nextSeq) {
+                state.nextSeq++;
+            }
+
+            if (object) {
+                if (object.stream) {
+                    reader.watch[object.seq] = object.stream;
+                    Oncemore(object.stream).once('end', 'error', function () {
+
+                        delete reader.watch[object.seq];
+                    });
+                }
+
+                state.active = reader.push(object);
+            }
+
+            internals.checkNext(reader);
+        });
+    } else if (index.ended) {
+        reader.push(null);
+    } else if (!index.type && (index.lastSeqNo() < state.nextSeq - 1)) {
+        // handle live stream restart
+        state.discont = true;
+        state.nextSeq = index.startSeqNo(true);
+        internals.checkNext(reader);
+    }
 };
 
 
 var HlsSegmentReader = function (src, options) {
 
-  if (!(this instanceof HlsSegmentReader)) {
-    return new HlsSegmentReader(src, options);
-  }
-
-  var self = this;
-
-  options = options || {};
-  if (typeof src === 'string') {
-    src = Url.parse(src);
-  }
-
-  this.url = src;
-  this.baseUrl = src;
-
-  this.fullStream = !!options.fullStream;
-  this.withData = !!options.withData;
-
-  // dates are inclusive
-  this.startDate = options.startDate ? new Date(options.startDate) : null;
-  this.stopDate = options.stopDate ? new Date(options.stopDate) : null;
-
-  this.maxStallTime = options.maxStallTime || Infinity;
-
-  this.extensions = options.extensions || {};
-
-  this.index = null;
-  this.readState = {
-    nextSeq: -1,
-    active: false,
-    fetching: null,
-    discont: false
-  };
-  this.watch = {}; // used to stop buffering on expired segments
-
-  this.indexStallSince = null;
-
-  var getUpdateInterval = function (updated) {
-
-    if (updated && self.index.segments.length) {
-      self.indexStallSince = null;
-      return Math.min(self.index.target_duration, self.index.segments[self.index.segments.length - 1].duration);
+    if (!(this instanceof HlsSegmentReader)) {
+        return new HlsSegmentReader(src, options);
     }
 
-    if (self.indexStallSince !== null) {
-      if ((Date.now() - self.indexStallSince) > self.maxStallTime) {
-        return -1;
-      }
-    } else {
-      self.indexStallSince = Date.now();
+    var self = this;
+
+    options = options || {};
+    if (typeof src === 'string') {
+        src = Url.parse(src);
     }
 
-    return self.index.target_duration / 2;
-  };
+    this.url = src;
+    this.baseUrl = src;
 
-  var initialSeqNo = function () {
+    this.fullStream = !!options.fullStream;
+    this.withData = !!options.withData;
 
-    var index = self.index;
+    // dates are inclusive
+    this.startDate = options.startDate ? new Date(options.startDate) : null;
+    this.stopDate = options.stopDate ? new Date(options.stopDate) : null;
 
-    if (!self.fullStream && self.startDate) {
-      return index.seqNoForDate(self.startDate, true);
-    }
+    this.maxStallTime = options.maxStallTime || Infinity;
 
-    return index.startSeqNo(self.fullStream);
-  };
+    this.extensions = options.extensions || {};
 
-  var updatecheck = function (updated) {
+    this.index = null;
+    this.readState = {
+        nextSeq: -1,
+        active: false,
+        fetching: null,
+        discont: false
+    };
+    this.watch = {}; // used to stop buffering on expired segments
 
-    if (updated) {
-      if (self.readState.nextSeq === -1) {
-        self.readState.nextSeq = initialSeqNo();
-      }
-      else if (self.readState.nextSeq < self.index.startSeqNo(true)) {
-        // playlist skipped ahead for whatever reason
-        self.readState.discont = true;
-        self.readState.nextSeq = self.index.startSeqNo(true);
-      }
+    this.indexStallSince = null;
 
-      var abortStream = function (stream) {
+    var getUpdateInterval = function (updated) {
 
-        if (!stream.ended && stream.abort) {
-          stream.abort();
-        }
-      };
-
-      // check watched segments
-      for (var seq in self.watch) {
-        if (!self.index.isValidSeqNo(seq)) {
-          var stream = self.watch[seq];
-          delete self.watch[seq];
-
-          setTimeout(abortStream, self.index.target_duration * 1000, stream);
-        }
-      }
-
-      self.emit('index', self.index);
-
-      if (self.index.variant) {
-        return self.push(null);
-      }
-    }
-    internals.checkNext(self);
-
-    if (self.index && !self.index.ended && self.readable) {
-      var updateInterval = getUpdateInterval(updated);
-      if (updateInterval <= 0) {
-        return self.emit('error', new Error('index stall'));
-      }
-
-      setTimeout(updateindex, Math.max(1, updateInterval) * 1000);
-    }
-  };
-
-  var updateindex = function () {
-
-    if (!self.readable) {
-      return;
-    }
-
-    var stream = UriStream(Url.format(self.url), { timeout: 30 * 1000 });
-    stream.on('meta', function (meta) {
-
-      // Check for valid mime type
-
-      if (self.indexMimeTypes.indexOf(meta.mime.toLowerCase()) === -1 &&
-          meta.url.indexOf('.m3u8', meta.url.length - 5) === -1 &&
-          meta.url.indexOf('.m3u', meta.url.length - 4) === -1) {
-
-        // FIXME: correctly handle .m3u us-ascii encoding
-        if (stream.abort) {
-          stream.abort();
+        if (updated && self.index.segments.length) {
+            self.indexStallSince = null;
+            return Math.min(self.index.target_duration, self.index.segments[self.index.segments.length - 1].duration);
         }
 
-        return stream.emit('error', new Error('Invalid MIME type: ' + meta.mime));
-      }
-
-      self.baseUrl = meta.url;
-    });
-
-    M3U8Parse(stream, { extensions: self.extensions }, function (err, index) {
-
-      if (!self.readable) {
-        return;
-      }
-
-      if (err) {
-        self.emit('error', err);
-        updatecheck(false);
-      } else {
-        var updated = true;
-        if (self.index && self.index.lastSeqNo() === index.lastSeqNo()) {
-          updated = false;
+        if (self.indexStallSince !== null) {
+            if ((Date.now() - self.indexStallSince) > self.maxStallTime) {
+                return -1;
+            }
+        } else {
+            self.indexStallSince = Date.now();
         }
 
-        self.index = index;
-        updatecheck(updated);
-      }
-    });
-  };
+        return self.index.target_duration / 2;
+    };
 
-  Readable.call(this, { objectMode: true, highWaterMark: options.highWaterMark || 0 });
+    var initialSeqNo = function () {
 
-  updateindex();
+        var index = self.index;
+
+        if (!self.fullStream && self.startDate) {
+            return index.seqNoForDate(self.startDate, true);
+        }
+
+        return index.startSeqNo(self.fullStream);
+    };
+
+    var updatecheck = function (updated) {
+
+        if (updated) {
+            if (self.readState.nextSeq === -1) {
+                self.readState.nextSeq = initialSeqNo();
+            }
+            else if (self.readState.nextSeq < self.index.startSeqNo(true)) {
+                // playlist skipped ahead for whatever reason
+                self.readState.discont = true;
+                self.readState.nextSeq = self.index.startSeqNo(true);
+            }
+
+            var abortStream = function (stream) {
+
+                if (!stream.ended && stream.abort) {
+                    stream.abort();
+                }
+            };
+
+            // check watched segments
+            for (var seq in self.watch) {
+                if (!self.index.isValidSeqNo(seq)) {
+                    var stream = self.watch[seq];
+                    delete self.watch[seq];
+
+                    setTimeout(abortStream, self.index.target_duration * 1000, stream);
+                }
+            }
+
+            self.emit('index', self.index);
+
+            if (self.index.variant) {
+                return self.push(null);
+            }
+        }
+        internals.checkNext(self);
+
+        if (self.index && !self.index.ended && self.readable) {
+            var updateInterval = getUpdateInterval(updated);
+            if (updateInterval <= 0) {
+                return self.emit('error', new Error('index stall'));
+            }
+
+            setTimeout(updateindex, Math.max(1, updateInterval) * 1000);
+        }
+    };
+
+    var updateindex = function () {
+
+        if (!self.readable) {
+            return;
+        }
+
+        var stream = UriStream(Url.format(self.url), { timeout: 30 * 1000 });
+        stream.on('meta', function (meta) {
+
+            // Check for valid mime type
+
+            if (self.indexMimeTypes.indexOf(meta.mime.toLowerCase()) === -1 &&
+                    meta.url.indexOf('.m3u8', meta.url.length - 5) === -1 &&
+                    meta.url.indexOf('.m3u', meta.url.length - 4) === -1) {
+
+                // FIXME: correctly handle .m3u us-ascii encoding
+                if (stream.abort) {
+                    stream.abort();
+                }
+
+                return stream.emit('error', new Error('Invalid MIME type: ' + meta.mime));
+            }
+
+            self.baseUrl = meta.url;
+        });
+
+        M3U8Parse(stream, { extensions: self.extensions }, function (err, index) {
+
+            if (!self.readable) {
+                return;
+            }
+
+            if (err) {
+                self.emit('error', err);
+                updatecheck(false);
+            } else {
+                var updated = true;
+                if (self.index && self.index.lastSeqNo() === index.lastSeqNo()) {
+                    updated = false;
+                }
+
+                self.index = index;
+                updatecheck(updated);
+            }
+        });
+    };
+
+    Readable.call(this, { objectMode: true, highWaterMark: options.highWaterMark || 0 });
+
+    updateindex();
 };
 Util.inherits(HlsSegmentReader, Readable);
 
 
 HlsSegmentReader.prototype.abort = function (graceful) {
 
-  if (!graceful) {
+    if (!graceful) {
 
-    // Abort all active streams
+        // Abort all active streams
 
-    if (this.fetching && !this.fetching.ended && this.fetching.abort) {
-      this.fetching.abort();
+        if (this.fetching && !this.fetching.ended && this.fetching.abort) {
+            this.fetching.abort();
+        }
+
+        for (var seq in this.watch) {
+            var stream = this.watch[seq];
+            delete this.watch[seq];
+            if (!stream.ended && stream.abort) {
+                stream.abort();
+            }
+        }
     }
 
-    for (var seq in this.watch) {
-      var stream = this.watch[seq];
-      delete this.watch[seq];
-      if (!stream.ended && stream.abort) {
-        stream.abort();
-      }
+    if (!this.readable) {
+        return;
     }
-  }
 
-  if (!this.readable) {
-    return;
-  }
+    if (!this._readableState.ended) {
+        this.push(null);
+    }
 
-  if (!this._readableState.ended) {
-    this.push(null);
-  }
-
-  this.readable = false;
+    this.readable = false;
 };
 
 
 HlsSegmentReader.prototype.destroy = function () {
 
-  return this.abort();
+    return this.abort();
 };
 
 
 HlsSegmentReader.prototype.indexMimeTypes = [
-  'application/vnd.apple.mpegurl',
-  'application/x-mpegurl',
-  'audio/mpegurl'
+    'application/vnd.apple.mpegurl',
+    'application/x-mpegurl',
+    'audio/mpegurl'
 ];
 
 
 HlsSegmentReader.prototype.segmentMimeTypes = [
-  'video/mp2t',
-  'audio/aac',
-  'audio/x-aac',
-  'audio/ac3',
-  'text/vtt'
+    'video/mp2t',
+    'audio/aac',
+    'audio/x-aac',
+    'audio/ac3',
+    'text/vtt'
 ];
 
 
 HlsSegmentReader.prototype._read = function (/*n*/) {
 
-  this.readState.active = true;
-  internals.checkNext(this);
+    this.readState.active = true;
+    internals.checkNext(this);
 };
 
 
@@ -360,8 +360,8 @@ var exports = module.exports = HlsSegmentReader;
 
 exports.HlsSegmentObject = function (seq, segment, fileMeta, stream) {
 
-  this.seq = seq;
-  this.details = segment;
-  this.file = fileMeta;
-  this.stream = stream;
+    this.seq = seq;
+    this.details = segment;
+    this.file = fileMeta;
+    this.stream = stream;
 };
