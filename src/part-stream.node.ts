3a23aa3b4b37e38cdfb26763f6dacae23dd3e769
Gil Pedersen <git@gpost.dk>
2023-03-15 10:11:53 +0100

    Fix stall issue

--- src/part-stream.node.ts
+++ src/part-stream.node.ts
@@ -1,15 +1,42 @@
-import type { ContentFetcher } from 'hls-playlist-reader/helpers';
+import { AbortError, assert, ContentFetcher } from 'hls-playlist-reader/helpers';
 
-import { Readable, PassThrough, finished } from 'stream';
-import { promisify } from 'util';
+import { Readable } from 'stream';
 
 import { IPartStream, PartStreamCtor, PartStreamOptions, partStreamSetup } from './part-stream.js';
 
-export class PartStream extends partStreamSetup<Readable, Omit<typeof PassThrough, 'new'> & PartStreamCtor<PassThrough>>(PassThrough as any) implements IPartStream {
+export class PartStream extends partStreamSetup<Readable, Omit<typeof Readable, 'new'> & PartStreamCtor<Readable>>(Readable as any) implements IPartStream {
+
+    #active?: Readable;
 
     constructor(fetcher: InstanceType<typeof ContentFetcher>, options: PartStreamOptions) {
 
         super(fetcher, options);
+    }
+
+    async #feedPart(stream: Readable, final: boolean): Promise<void> {
+
+        assert(!this.#active);
+
+        try {
+            this.#active = stream;
+
+            for await (const chunk of stream) {
+                if (this.destroyed) {
+                    return;
+                }
+
+                if (!this.push(chunk)) {
+                    stream.pause();
+                }
+            }
+
+            if (final) {
+                this.push(null);
+            }
+        }
+        finally {
+            this.#active = undefined;
+        }
     }
 
     _feedPart(err?: Error, stream?: Readable, final?: boolean): Promise<void> | void {
@@ -20,14 +47,19 @@ export class PartStream extends partStreamSetup<Readable, Omit<typeof PassThroug
         }
 
         if (stream) {
-            stream.pipe(this, { end: final });
-
-            return promisify(finished)(stream);
+            return this.#feedPart(stream, !!final);
         }
     }
 
     cancel(reason?: Error) {
 
         this.destroy(reason);
+        this.#active?.destroy(new AbortError('cancelled', { cause: reason }));
+        this.#active = undefined;
+    }
+
+    _read() {
+
+        this.#active?.resume();
     }
 }
